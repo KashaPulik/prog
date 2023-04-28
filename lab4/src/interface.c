56b30849e474b666f48fe4c0aba0e53c41278847
Kulik Pavel <KashaPulik@users.noreply.github.com>
2023-04-29 02:01:43 +0700

    Add error_output

--- lab4/src/interface.c
+++ lab4/src/interface.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lexer.h"
+#include "interface.h"
 
 char* input(char* delim)
 {
@@ -18,4 +20,21 @@ char* input(char* delim)
 void output(char* new_paths)
 {
     printf("new paths: %s\n", new_paths);
+}
+
+void error_output(char *str, int exit_code)
+{
+    printf("%s\n", str);
+    printf(RED"Error: "NONE);
+    switch (exit_code) {
+        case OVER_MAX_LEN:
+            printf("length of path is higher than 260 symbols\n");
+            break;
+        case ILLEGAL_CHARACTER:
+            printf("wrong symbol detected\nYou can't use \\*?«<>| in the path\n");
+            break;
+        case WRONG_IP_DOMEN:
+            printf("IP or domen aren't correct\n");
+            break;
+    }
 }
